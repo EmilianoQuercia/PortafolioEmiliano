19e690b93c37308b4a0a958b12d59466ca66affd
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-01-19 21:06:37 -0300

    Agregue nuevo proyecto y modifique con un map la carga de los mismos

--- src/components/projects/Proyectos.jsx
+++ src/components/projects/Proyectos.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './proyectos.css';
 
-const Proyectos = () => {
+const Proyectos =  () => {
+
+    const[db, setDb] = useState([])
+
+    const proyectos = async()=>{
+
+        const response = await fetch('src/data/projects.json')
+        const data = await response.json()
+        setDb(data)
+    }
+    useEffect(() => {
+   
+        proyectos()
+    }, []);
+    
+
     return (
         <div className='containerProyectos'>
             <div id='proyect' className="separacion"></div>
@@ -10,60 +25,26 @@ const Proyectos = () => {
             <br />
             <h1  className='tituloProyectos'>PROYECTOS</h1>
             <div className='containerCard '>
-                <div className='cardProyect'>
+                {db.map((date,index)=>(
+                    <div key={index} className='cardProyect'>
                     <div className='imgCard'>
-                        <img src="./assets/img/portadaAromaticas.jpg" alt="" />
+                        <img src={date.img} alt="" />
                     </div>
                     <div className='infoCard'>
-                        <h3>App Aromaticas</h3>
-                        <p>Es una web para introducirse en el mundo de las hierbas aromáticas. Primer trabajo en equipo</p>
-                        <p>TECNOLOGIAS: HTML, CSS, BOOSTRAPT, JAVASCRIPT, REACT</p>
+                        <h3>{date.title}</h3>
+                        <p>{date.description}</p>
+                        <p>TECNOLOGIAS: {date.tech}</p>
                     </div>
                     <div className='containerBtn'>
-                        <a className='linkBtn' href="https://github.com/ProjectsDevLatam/AromaticsApp" target="_blank" rel="noopener noreferrer">
+                        <a className='linkBtn' href={date.linkGithub} target="_blank" rel="noopener noreferrer">
                             <button className='btnCard '> <img className='iconoBtn' src="./assets/img/iconoGithub.png" alt="" />Github</button>
                         </a>
-                        <a className='linkBtn' href="https://aromatics.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <a className='linkBtn' href={date.linkWeb} target="_blank" rel="noopener noreferrer">
                             <button className='btnCard '><img className='iconoBtn' src="./assets/img/iconoWeb.png" alt="" />Demo</button>
                         </a>
                     </div>
                 </div>
-                <div className='cardProyect'>
-                    <div className='imgCard'>
-                        <img src="./assets/img/portadaMentalmente.jpg" alt="" />
-                    </div>
-                    <div className='infoCard'>
-                        <h3>Mentalmente</h3>
-                        <p>Es un juego de Preguntas y respuestas con dos modalidades de Juego, tabla de puntaje final, musica y sonido.</p>
-                        <p>TECNOLOGIAS: HTML, CSS, JAVASCRIPT, REACT </p>
-                    </div>
-                    <div className='containerBtn'>
-                        <a className='linkBtn' href="https://github.com/EmilianoQuercia/Mentalmente/tree/main/Mentalmente" target="_blank" rel="noopener noreferrer">
-                            <button className='btnCard '> <img className='iconoBtn' src="./assets/img/iconoGithub.png" alt="" />Github</button>
-                        </a>
-                        <a className='linkBtn' href="https://mentalmente.vercel.app/" target="_blank" rel="noopener noreferrer">
-                            <button className='btnCard '><img className='iconoBtn' src="./assets/img/iconoWeb.png" alt="" />Demo</button>
-                        </a>
-                    </div>
-                </div>
-                <div className='cardProyect'>
-                    <div className='imgCard'>
-                        <img src="./assets/img/portadaBeat.jpg" alt="" />
-                    </div>
-                    <div className='infoCard'>
-                        <h3>Beat Sound</h3>
-                        <p>Este fue la primer pagina que hice, un sitio para el alquiler de equipos y organizacion de eventos</p>
-                        <p>TECNOLOGIAS: HTML, CSS, BOOSTRAPS, SASS</p>
-                    </div>
-                    <div className='containerBtn'>
-                        <a className='linkBtn' href="https://github.com/EmilianoQuercia/BeatSound" target="_blank" rel="noopener noreferrer">
-                            <button className='btnCard '> <img className='iconoBtn' src="./assets/img/iconoGithub.png" alt="" />Github</button>
-                        </a>
-                        <a className='linkBtn' href="https://emilianoquercia.github.io/BeatSound/" target="_blank" rel="noopener noreferrer">
-                            <button className='btnCard '><img className='iconoBtn' src="./assets/img/iconoWeb.png" alt="" />Demo</button>
-                        </a>
-                    </div>
-                </div>
+                ))}
             </div>
             
         </div>
